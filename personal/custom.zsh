c1ac320dffa320a165148c989ca33b2b51f15567
Adam Archer <agarcher@gmail.com>
2022-01-15 22:37:12 -0500

    add git aliases

--- personal/custom.zsh
+++ personal/custom.zsh
@@ -2,13 +2,32 @@
 # This file is loaded after everything else, but before Antigen is applied and ~/extra.zsh sourced.
 # Put anything here that you want to exist on all your environment, and to have the highest priority
 # over any other customization.
+
+# Default to nvim
 export EDITOR=nvim
 
 # load powerlevel10k prompt theme
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
+# Use vi mode in zsh
 bindkey -v
+
+# Aliases
+alias reload="source ~/.zshrc"
+
+alias g="git"
+alias gl='git pull --prune'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp='git push'
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gam='git commit --amend'
+alias gamn='git commit --amend --no-edit'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status -sb'
 
 if [ "$ZSH_HOST_OS" = "darwin" ]; then
   # load fzf keybindings
