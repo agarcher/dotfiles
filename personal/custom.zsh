e86a919976925aa8602ffc98accb8eb240ceede8
Adam Archer <agarcher@gmail.com>
2022-01-15 19:32:32 -0500

    enable (improved) vi mode for zsh

--- personal/custom.zsh
+++ personal/custom.zsh
@@ -8,6 +8,8 @@ export EDITOR=nvim
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
+bindkey -v
+
 if [ "$ZSH_HOST_OS" = "darwin" ]; then
   # load fzf keybindings
   [ -e ~/.fzf.zsh ] && source ~/.fzf.zsh
